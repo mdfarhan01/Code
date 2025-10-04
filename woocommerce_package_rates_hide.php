ef04c718d2032e8130868c03dd7d080eecd03da7
Md Farhan Sadik <shemonsm01@gmail.com>
2025-10-04 08:34:42 +0600

    Update woocommerce_package_rates_hide.php

--- woocommerce_package_rates_hide.php
+++ woocommerce_package_rates_hide.php
@@ -26,4 +26,37 @@ function custom_shipping_logic_based_on_cart_total( $rates, $package ) {
 }
 
 
+
+
+
+//***************************** Pickup location hide in a minimum amount ********************************************** 
+
+
+add_filter( 'woocommerce_package_rates', 'custom_shipping_logic_based_on_cart_total', 10, 2 );
+function custom_shipping_logic_based_on_cart_total( $rates, $package ) {
+    $cart_total = WC()->cart->get_subtotal();
+
+    // If cart total >= 50, hide Local Pickup
+    if ( $cart_total >= 50 ) {
+        foreach ( $rates as $rate_id => $rate ) {
+            if ( 'local_pickup' === $rate->method_id ) {
+                unset( $rates[$rate_id] );
+            }
+        }
+    }
+
+    // If cart total < 50, hide Free Shipping
+    if ( $cart_total < 50 ) {
+        foreach ( $rates as $rate_id => $rate ) {
+            if ( 'free_shipping' === $rate->method_id ) {
+                unset( $rates[$rate_id] );
+            }
+        }
+    }
+
+    return $rates;
+}
+
+
+
 ?>
